6119de7f21a385e3d50cb7bddaa14e0af59da9cc
Vlad Nicoara <vladcristian.n@gmail.com>
2018-02-16 15:31:22 +0200

    Add lombok logger and extension methods (for fun now but could be useful in clean coding later on).
    Signed-off-by: Vlad Nicoara <vlad.nicoara@elan-solutions.eu>

--- WorkerInstance/src/main/java/com/example/demo/WebConfiguration.java
+++ WorkerInstance/src/main/java/com/example/demo/WebConfiguration.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.experimental.ExtensionMethod;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +17,9 @@ import java.util.List;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
 @Configuration
+@ExtensionMethod({ExtensionUtilMethods.class})
+@Log4j2
 public class WebConfiguration {
-    private static final Logger logger = LoggerFactory.getLogger(WebConfiguration.class);
 
     private RestTemplateBuilder restTemplateBuilder;
     private GoogleCloudConfigurationBean gcConfiguration;
@@ -35,9 +36,10 @@ public class WebConfiguration {
      */
     @Bean
     RouterFunction<?> routes() {
-        List<Employee> employeeList = List.of(new Employee(0, "ListEmployee0"),
-                                              new Employee(1, "ListEmployee1"),
-                                              new Employee(2, "ListEmployee2"));
+        List<Employee> employeeList = List.of(
+//                new Employee(0, "listEmployee0").capitalize(),
+                new Employee(1, "ListEmployee1"),
+                new Employee(2, "ListEmployee2"));
 
         final RouterFunction<ServerResponse> route = RouterFunctions
                 .route(GET("/employees"),
@@ -60,7 +62,7 @@ public class WebConfiguration {
                                       .body(Mono.justOrEmpty(serverResponseHandlerFunction.getBody()),
                                             String.class);
                           });
-        logger.debug("Done setting up /employees endpoint.");
+        log.info("Done setting up /employees endpoint.");
         return route;
     }
 }
